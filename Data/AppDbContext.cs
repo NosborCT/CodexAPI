c1298a134ae4d7df88c5f4f282afa97f0528c447
NosborCT <hainiken08@gmail.com>
2024-11-19 15:50:45 -0300

    Fix: Arrumando a sintaxe que estava errada e adicionando as usages

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using CodexAPI.Models;
 
 namespace CodexAPI.Data
 {
     public class AppDbContext : DbContext
-
     {
         /* para que o entityframework reconheça as classes que serão usadas no
-         * banco de dados temos que criar um construtor que recebe um AppDbContextOptions como parâmetro */ 
-        public AppDbContext(AppDbContextOptions <AppDbContext> options) : base(options)
+         * banco de dados temos que criar um construtor que recebe um AppDbContextOptions como parâmetro */
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
         }
-        // mapeando os modelos para as tabelas
-        public Dbset <AutorModel> Autores { get; set; }
-        public Dbset <LiuvroModel> Livros { get; set; }
 
+        // Mapeamento dos modelos para as tabelas
+        public DbSet<AutorModel> Autores { get; set; }
+        public DbSet<LivroModel> Livros { get; set; }
     }
 }
