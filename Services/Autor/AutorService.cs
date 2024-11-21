7dde73760a15f4882287c3b9ca996eece35be3a6
NosborCT <hainiken08@gmail.com>
2024-11-21 00:39:27 -0300

    Feat: Criado o service do autor e já implementado a task de listarAutores

---
+++ Services/Autor/AutorService.cs
@@ -0,0 +1,46 @@
+﻿using CodexAPI.Data;
+using CodexAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodexAPI.Services.Autor
+{
+    public class AutorService : IAutorInterface
+    {
+        private readonly AppDbContext _context;
+        public AutorService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<ResponseModel<AutorModel>> BuscarAutorPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
+        public async Task<ResponseModel<List<AutorModel>>> ListarAutores()
+        {
+            ResponseModel<List<AutorModel>> resposta = new ResponseModel<List<AutorModel>>();
+            try
+            {
+                var autores = await _context.Autores.ToListAsync();
+                resposta.Dados = autores;
+                resposta.Mensagem = "Autores listados com sucesso";
+                return resposta;
+
+            }
+            catch (Exception ex)
+            {
+                
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
+    }
+}
