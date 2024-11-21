4bcaf209fa6ad9282d47e0da2171cb4d9f0417c7
NosborCT <hainiken08@gmail.com>
2024-11-21 00:38:27 -0300

    Feat: Criado o controller e adicionado o GET de busca de autores

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using CodexAPI.Services.Autor;
+using CodexAPI.Models;
+
+namespace codexapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private readonly IAutorInterface _autorInterface;
+        public AutorController(IAutorInterface autorInterface)
+        {
+            _autorInterface = autorInterface;
+        }
+        [HttpGet("ListarAutores")] // api/Autor/ListarAutores
+        public async Task<ActionResult<ResponseModel<List<AutorModel>>>>ListarAutores(){
+            var autores = await _autorInterface.ListarAutores();
+            return Ok(autores);
+        }
+    }
+}
