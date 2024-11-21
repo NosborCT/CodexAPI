d12db058a4565f5e15115a0d539f1bd888fb2a9a
NosborCT <hainiken08@gmail.com>
2024-11-21 00:38:46 -0300

    Feat: Criado o modelo de resposta

---
+++ Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CodexAPI.Models
+{
+    public class ResponseModel <T>
+    {
+        public T? Dados { get; set; }
+        public bool Status { get; set; } = true;
+        public string Mensagem { get; set; } = string.Empty;
+    }
+}
